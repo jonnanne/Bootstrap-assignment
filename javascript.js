fa294c174b3bf730a032ac6c9fee4b5a05c8eca8
jonnanne <101732110+jonnanne@users.noreply.github.com>
2022-04-12 12:49:05 +0300

    uodated tunnista-page added js-file

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.getElementById("wrong").onclick = function () {
+    alert("Ei näin!");
+}
+
+document.getElementById("right").onclick = function () {
+    alert("Oikein! Todennäköisesti työkaverisi on saanut sairauskohtauksen ja tarvitsee ammattiapua.")
+}
