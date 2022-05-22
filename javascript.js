1fca018772dfe59d33fa8e403c6028296a08a16c
jonnanne <101732110+jonnanne@users.noreply.github.com>
2022-05-22 20:53:24 +0300

    muokattu javascript.js kansiota > lisätty API sekä halytaApua kansion sisältöä

--- javascript.js
+++ javascript.js
@@ -1,6 +1,22 @@
 
-//firstPage sivulla kyselyn nappuloiden toiminnot (alertilla oikea/väärä vastaus) sekä boxin sulkeminen
+//halytaApua-sivun API
+fetch('https://friends-quotes-api.herokuapp.com/quotes')
+.then(response => response.json())
+.then(data => {
+    const TEST_BTN = document.getElementById("testaa");
 
+TEST_BTN.addEventListener("click",showAPI);
+
+function showAPI() {
+    document.getElementById("testi").innerHTML = "<h4>" + data[10].quote + "</h4>";
+}
+}
+);
+
+
+
+
+//firstPage sivulla kyselyn nappuloiden toiminnot (alertilla oikea/väärä vastaus) sekä boxin sulkeminen
 document.getElementById("wrong").onclick = function () {
     document.getElementById("alertWrong").style.display = "block";
 }
@@ -16,6 +32,4 @@ document.getElementById("right").onclick = function () {
 
 document.getElementById("closeRight").onclick = function () {
     document.getElementById("alertRight").remove();
-
 }
-
